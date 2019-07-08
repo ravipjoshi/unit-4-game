88e76cfecf45599d64b0cf3ac6ec537feb3e91b8
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-07-08 12:20:58 -0400

    updated index.html with skeleton of divs and display basic structure wihout css and initialized variables in game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+var hexagone = document.getElementById("hexagoneCrystal");
+var diamond= document.getElementById("diamon");
+var octagone = document.getElementById("octagoneCrystal");
+var square = document.getElementById("squareCrystal");
+var 
+
+var hexaval = 6;
+var dimval= 1;
+var octaval= 8;
+var sqrval= 4;
+
+$(window).ready(function(){
+
+    $(hexagone).on("click",function(){
+
+    });
+
+});
